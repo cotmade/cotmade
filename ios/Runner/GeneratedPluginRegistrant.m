d1ae8f808c88b767c7d661dcb0e307801ae83c7c
cotmade <timothy.o@cotmade.com>
2025-04-15 09:38:20 +0100

    Fixed cloud_firestore plugin registration in GeneratedPluginRegistrant.m

--- ios/Runner/GeneratedPluginRegistrant.m
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -1,14 +1,11 @@
-//
-//  Generated file. Do not edit.
-//
-
-// clang-format off
-
 #import "GeneratedPluginRegistrant.h"
+#import <cloud_firestore/CloudFirestorePlugin.h>  // Import the cloud_firestore plugin
 
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [CloudFirestorePlugin registerWithRegistrar:[registry registrarForPlugin:@"CloudFirestorePlugin"]];  // Register the plugin
 }
 
 @end
+
