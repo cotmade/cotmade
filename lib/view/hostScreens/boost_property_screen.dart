6d9e3c9f5a154327aa6705bd663a405d6f333d08
cotmade <timothy.o@cotmade.com>
2025-05-02 17:07:44 +0100

    added apple IAP

--- lib/view/hostScreens/boost_property_screen.dart
+++ lib/view/hostScreens/boost_property_screen.dart
@@ -3,10 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cotmade/model/app_constants.dart';
-import 'package:flutterwave_standard/flutterwave.dart';
 import 'package:get/get.dart';
 import 'dart:io';
 import 'package:cotmade/view/hostScreens/boost_success_screen.dart';
+import 'package:in_app_purchase/in_app_purchase.dart';
 
 class BoostPropertyPage extends StatefulWidget {
   final String postingId; // Passing postingId from the previous screen
@@ -21,10 +21,21 @@ class _BoostPropertyPageState extends State<BoostPropertyPage> {
   double amountInConvertedCurrency = 0.0;
   String postingName = ""; // Variable to store the posting name
 
+  // Define the customer object
+  final Customer customer = Customer(
+    email: AppConstants.currentUser.email.toString(),
+    name: AppConstants.currentUser.getFullNameOfUser(),
+    phoneNumber: AppConstants.currentUser.mobileNumber.toString(),
+  );
+
+  late final InAppPurchase _inAppPurchase;
+  late final ProductDetails _productDetails;
+
   @override
   void initState() {
     super.initState();
     _fetchCurrencyAndConvertAmount();
+    _initializeInAppPurchase();
   }
 
   Future<void> _fetchCurrencyAndConvertAmount() async {
@@ -36,8 +47,7 @@ class _BoostPropertyPageState extends State<BoostPropertyPage> {
     if (postingSnapshot.exists) {
       setState(() {
         currency = postingSnapshot['currency'];
-        postingName =
-            postingSnapshot['name']; // Assuming 'name' is a field in Firestore
+        postingName = postingSnapshot['name']; // Assuming 'name' is a field in Firestore
       });
 
       double usdAmount = 10.0;
@@ -48,8 +58,7 @@ class _BoostPropertyPageState extends State<BoostPropertyPage> {
     }
   }
 
-  Future<double> _getConversionRate(
-      String fromCurrency, String toCurrency) async {
+  Future<double> _getConversionRate(String fromCurrency, String toCurrency) async {
     final String apiKey = '65ecc5642a4b0653f9777381';
     final String url =
         'https://v6.exchangerate-api.com/v6/$apiKey/latest/$fromCurrency';
@@ -64,69 +73,64 @@ class _BoostPropertyPageState extends State<BoostPropertyPage> {
     }
   }
 
-  final Customer customer = Customer(
-      email: AppConstants.currentUser.email.toString(),
-      name: AppConstants.currentUser.getFullNameOfUser(),
-      phoneNumber: AppConstants.currentUser.mobileNumber.toString());
-
-  Future<void> _startPaymentProcess() async {
-    final Flutterwave flutterwave = Flutterwave(
-      context: context,
-      publicKey: 'FLWPUBK-5075e726729201f3c2b77df72b4a8da5-X',
-      currency: currency,
-      redirectUrl: 'https://cotmade.com',
-      txRef: DateTime.now().millisecondsSinceEpoch.toString(),
-      amount: amountInConvertedCurrency.toString(),
-      customer: customer,
-      paymentOptions: "card, payattitude, barter, bank transfer, ussd",
-      customization: Customization(title: "Live Payment"),
-      isTestMode: false,
-    );
-
-    final ChargeResponse response = await flutterwave.charge();
-    showLoading(response.toString());
-    print("Response: ${response.toJson()}");
-    print("Status: ${response.status}");
-
-    if (response.success == true || response.status == 'successful') {
-      DateTime paidDate = DateTime.now();
-      DateTime premiumExpiryDate = paidDate.add(Duration(days: 30));
-
-      FirebaseFirestore.instance.collection('premium').add({
-        'postingId': widget.postingId,
-        'paidAmount': amountInConvertedCurrency,
-        'paymentDate': paidDate,
-        'expiryDate': premiumExpiryDate,
+  // Initialize In-App Purchase
+  void _initializeInAppPurchase() async {
+    _inAppPurchase = InAppPurchase.instance;
+    final ProductDetailsResponse productDetailsResponse =
+        await _inAppPurchase.queryProductDetails({'com.cotmade.cotmade'}.toSet());
+    if (productDetailsResponse.error == null && productDetailsResponse.productDetails.isNotEmpty) {
+      setState(() {
+        _productDetails = productDetailsResponse.productDetails.first;
       });
-
-      FirebaseFirestore.instance
-          .collection('postings')
-          .doc(widget.postingId)
-          .update({
-        'premium': 2,
-      });
-
-      // Format the dates as strings
-      String paidDateString = paidDate.toIso8601String();
-      String premiumExpiryDateString = premiumExpiryDate.toIso8601String();
-      String postId = widget.postingId.toString();
-      String email = AppConstants.currentUser.email.toString();
-      String money = amountInConvertedCurrency.toString();
-
-      await sendWelcomeEmail(
-          postId, paidDateString, premiumExpiryDateString, money, email);
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Payment successful!')),
-      );
-      Get.off(BoostScreen());
-    } else {
-      ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text('Payment failed! Please try again.')));
     }
   }
 
-  Future<void> sendWelcomeEmail(String postId, String paidDateString,
-      String premiumExpiryDateString, String money, String email) async {
+  // Start the payment process using Apple In-App Purchase
+  Future<void> _startPaymentProcess() async {
+    if (_productDetails != null) {
+      final PurchaseParam purchaseParam = PurchaseParam(
+        productDetails: _productDetails,
+        applicationUserName: customer.email, // User's email
+      );
+
+      final bool success = await _inAppPurchase.buyNonConsumable(purchaseParam: purchaseParam);
+      if (success) {
+        DateTime paidDate = DateTime.now();
+        DateTime premiumExpiryDate = paidDate.add(Duration(days: 30));
+
+        FirebaseFirestore.instance.collection('premium').add({
+          'postingId': widget.postingId,
+          'paidAmount': amountInConvertedCurrency,
+          'paymentDate': paidDate,
+          'expiryDate': premiumExpiryDate,
+        });
+
+        FirebaseFirestore.instance.collection('postings').doc(widget.postingId).update({
+          'premium': 2,
+        });
+
+        // Format the dates as strings
+        String paidDateString = paidDate.toIso8601String();
+        String premiumExpiryDateString = premiumExpiryDate.toIso8601String();
+        String postId = widget.postingId.toString();
+        String email = AppConstants.currentUser.email.toString();
+        String money = amountInConvertedCurrency.toString();
+
+        await sendWelcomeEmail(postId, paidDateString, premiumExpiryDateString, money, email);
+
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Payment successful!')),
+        );
+        Get.off(BoostScreen());
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Payment failed! Please try again.')),
+        );
+      }
+    }
+  }
+
+  Future<void> sendWelcomeEmail(String postId, String paidDateString, String premiumExpiryDateString, String money, String email) async {
     final url = Uri.parse("https://cotmade.com/app/send_email_premium.php");
 
     final response = await http.post(url, body: {
@@ -162,7 +166,6 @@ class _BoostPropertyPageState extends State<BoostPropertyPage> {
   }
 
   String _formatAmount(double amount) {
-    // Format the amount with commas as a thousand separator
     return amount.toStringAsFixed(2).replaceAllMapped(
         RegExp(r'(\d)(?=(\d{3})+\.)'), (Match m) => '${m[1]},');
   }
@@ -176,12 +179,10 @@ class _BoostPropertyPageState extends State<BoostPropertyPage> {
       body: Center(
         child: amountInConvertedCurrency > 0.0
             ? Align(
-                alignment:
-                    Alignment.topCenter, // This aligns the container to the top
+                alignment: Alignment.topCenter,
                 child: Container(
                   padding: EdgeInsets.all(20),
-                  height: MediaQuery.of(context).size.height /
-                      3, // Set container height to half of the screen height
+                  height: MediaQuery.of(context).size.height / 3,
                   decoration: BoxDecoration(
                     color: Colors.white,
                     borderRadius: BorderRadius.circular(15),
@@ -234,16 +235,10 @@ class _BoostPropertyPageState extends State<BoostPropertyPage> {
                             ElevatedButton(
                               onPressed: _startPaymentProcess,
                               style: ElevatedButton.styleFrom(
-                                backgroundColor:
-                                    Colors.black, // Black background
-                                foregroundColor:
-                                    Colors.yellow[600], // Gold text color
-                                padding: EdgeInsets.symmetric(
-                                    vertical: 8,
-                                    horizontal:
-                                        16), // Adjust padding for small size
-                                minimumSize: Size(150,
-                                    40), // Set a small fixed size (optional)
+                                backgroundColor: Colors.black,
+                                foregroundColor: Colors.yellow[600],
+                                padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                                minimumSize: Size(150, 40),
                               ),
                               child: Text('Pay Now'),
                             ),
@@ -252,9 +247,23 @@ class _BoostPropertyPageState extends State<BoostPropertyPage> {
                       ),
                     ],
                   ),
-                ))
+                ),
+              )
             : CircularProgressIndicator(),
       ),
     );
   }
+}
+
+// Customer class definition
+class Customer {
+  final String email;
+  final String name;
+  final String phoneNumber;
+
+  Customer({
+    required this.email,
+    required this.name,
+    required this.phoneNumber,
+  });
 }
