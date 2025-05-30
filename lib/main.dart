59856d07e6bd9b86410bf24b1b504ddeb799e162
cotmade <timothy.o@cotmade.com>
2025-05-30 19:28:29 +0100

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,6 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:cotmade/view/onboarding_screen.dart';
 import 'package:cotmade/view/suspended_account_screen.dart';
 import 'package:cotmade/view/video_reels_screen.dart';
-import 'package:cotmade/view/video_reels_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
