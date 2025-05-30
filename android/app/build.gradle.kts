bf3e804c9ecf43f737013d03ad60853c4efe0053
cotmade <timothy.o@cotmade.com>
2025-05-30 19:20:24 +0100

    work flow and build gradle

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,14 +1,35 @@
-plugins {
-    id("com.android.application")
-    id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
-    id("dev.flutter.flutter-gradle-plugin")
-}
-
 android {
-    namespace = "com.cotmade.cotmade"
     compileSdk = 35
-    ndkVersion = flutter.ndkVersion
+    namespace = "com.cotmade.cotmade"
+
+    defaultConfig {
+        applicationId = "com.cotmade.cotmade"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 17
+        versionName = "1.0.0"
+    }
+
+    signingConfigs {
+        create("release") {
+            storeFile = file("keystore.jks")
+            storePassword = System.getenv("KEYSTORE_PASSWORD")
+            keyAlias = System.getenv("KEY_ALIAS")
+            keyPassword = System.getenv("KEY_PASSWORD")
+        }
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            isShrinkResources = false
+            signingConfig = signingConfigs.getByName("release")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -18,38 +39,4 @@ android {
     kotlinOptions {
         jvmTarget = JavaVersion.VERSION_11.toString()
     }
-
-    defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.cotmade.cotmade"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 24
-        targetSdk = 35
-        versionCode = 17
-        versionName = "1.0.0"
-    }
-
-    signingConfigs {
-    release {
-        storeFile file("keystore.jks")
-        storePassword System.getenv("KEYSTORE_PASSWORD")
-        keyAlias System.getenv("KEY_ALIAS")
-        keyPassword System.getenv("KEY_PASSWORD")
-    }
-}
-
-buildTypes {
-    release {
-        signingConfig signingConfigs.release
-        minifyEnabled false
-        shrinkResources false
-        proguardFiles getDefaultProguardFile('proguard-android-optimize.txt'), 'proguard-rules.pro'
-    }
-}
-
-}
-
-flutter {
-    source = "../.."
 }
