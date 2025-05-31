645ac06425b058db9dd58df4da8a7f189162cd5a
cotmade <timothy.o@cotmade.com>
2025-05-31 14:14:24 +0100

    gradle

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,32 +1,37 @@
+// Root-level build.gradle.kts
+
+// Set the Kotlin version globally
+extra["kotlin_version"] = "1.8.0"
+
 buildscript {
-    ext.kotlin_version = '1.8.0'
     repositories {
+        // Google's Maven repository
         google()
+        // Maven Central repository
         mavenCentral()
     }
 
     dependencies {
-	classpath 'com.android.tools.build:gradle:7.3.0'
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
-        classpath 'com.google.gms:google-services:4.3.15'  // Add this line
+        // Android Gradle Plugin
+        classpath("com.android.tools.build:gradle:7.3.0")
+
+        // Kotlin Gradle Plugin
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${extra["kotlin_version"]}")
+
+        // Google Services Plugin (for Firebase)
+        classpath("com.google.gms:google-services:4.3.15")
     }
 }
 
 allprojects {
     repositories {
+        // Google's Maven repository
         google()
+        // Maven Central repository
         mavenCentral()
     }
 }
 
-rootProject.buildDir = '../build'
-subprojects {
-    project.buildDir = "${rootProject.buildDir}/${project.name}"
-}
-subprojects {
-    project.evaluationDependsOn(':app')
-}
-
-tasks.register("clean", Delete) {
-    delete rootProject.buildDir
+task<Delete>("clean") {
+    delete(rootProject.buildDir)
 }
