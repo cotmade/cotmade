837c9f80b4bf51f4c128d7c87ed794aec7d97fca
cotmade <timothy.o@cotmade.com>
2025-06-06 22:03:33 +0100

    upload

--- lib/view/hostScreens/video_upload.dart
+++ lib/view/hostScreens/video_upload.dart
@@ -8,7 +8,6 @@ import 'package:video_compress/video_compress.dart';
 import 'package:cotmade/model/posting_model.dart';
 import 'package:cotmade/model/app_constants.dart';
 import 'package:video_player/video_player.dart';
-import 'package:flutter_sound/flutter_sound.dart';
 import 'package:cotmade/model/app_constants.dart';
 import 'package:just_audio/just_audio.dart';
 
