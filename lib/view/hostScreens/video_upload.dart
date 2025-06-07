3bfbe57c6f697900124f3cf91e4814c16674ae42
cotmade <timothy.o@cotmade.com>
2025-06-07 08:26:59 +0100

    upload

--- lib/view/hostScreens/video_upload.dart
+++ lib/view/hostScreens/video_upload.dart
@@ -7,7 +7,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:video_compress/video_compress.dart';
 import 'package:cotmade/model/app_constants.dart';
 import 'package:video_player/video_player.dart';
-import 'package:flutter_sound/flutter_sound.dart';
+//import 'package:flutter_sound/flutter_sound.dart';
 import 'package:cotmade/model/app_constants.dart';
 import 'package:just_audio/just_audio.dart';
 
