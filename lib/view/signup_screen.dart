5ecd64592478e6ed50cb0230724fa8e539f45435
cotmade <timothy.o@cotmade.com>
2025-07-21 17:35:30 +0100

    signup

--- lib/view/signup_screen.dart
+++ lib/view/signup_screen.dart
@@ -1213,7 +1213,7 @@ class _SignupScreenState extends State<SignupScreen> {
               ),
             ),
 
-            //keeping for tab
+            //keeping for tabs
             //  Padding(
             //    padding: const EdgeInsets.only(top: 22.0),
             //    child: MaterialButton(
